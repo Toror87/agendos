467c4969dc7fc5ac051f5bae4f5049b93b6c8a2b
antoinsa <tu>
2021-11-06 17:56:17 -0300

    perfil-contacto

---
+++ frontend/frontend/src/perfil-contacto.js
@@ -0,0 +1,40 @@
+
+<div class="container">
+          <header class="hero">
+              <a href="index.html">
+                <i class="fas fa-arrow-circle-left back-btn"></i>
+              </a>
+              <div class="hero-info">
+                  <h1 class="nombre">Antonella Insaurralde</h1>
+                  <p class="relacion">Amigo/a</p>
+              </div>
+          </header>
+
+          <section class="info-contacto">
+              <div class="informacion">
+                <i class="fas fa-phone icon-gradient" t></i>
+                  <p class="telefono">000-000-0000</p>
+              </div>
+
+              <div class="informacion">
+                <i class="fas fa-sms icon-gradient" ></i>
+                <p class="SMS">Enviar mensaje</p>
+            </div>
+            <div class="informacion">
+                <i class="fas fa-at icon-gradient" ></i>
+                <p class="mail">alguien@gmail.com</p>
+            </div>
+            <div class="informacion">
+                <i id="location" class="fas fa-map-marked-alt icon-gradient" ></i>
+                <p class="direccion"> Calle 1234 depto 2</p>
+            </div>
+          </section>
+
+          <a href="#">
+              <section class="editar-contacto">
+                  <div class="editar-boton">
+                    <i class="fas fa-cog icon-gradient"></i>
+                  </div>
+              </section>
+          </a>
+      </div>
